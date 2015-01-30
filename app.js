51371dcb12f6fb4d36840b78b91e91ea29ebe0c8
Christopher Brandt <xtopher.brandt@gmail.com>
2015-01-30 22:14:31 +0000

    Completed basic fingerprint generation

--- app.js
+++
@@ -1,4 +0,0 @@
-var main = function (){
-};
-
-$(document).ready(main);
--- winebook.py
+++ winebook.py
@@ -3,15 +3,14 @@ import os
 import urllib
 import logging
 import datetime
-import json
-import re
 
 from google.appengine.api import users
 from google.appengine.ext import ndb
 from google.appengine.api import urlfetch
 from google.appengine.api import taskqueue
-from ecdsa import SigningKey
-from ecdsa.util import PRNG
+from ecdsa.ecdsa import SigningKey
+from ecdsa.ecdsa.util import PRNG
+from base58 import base58
 
 import jinja2
 import webapp2
@@ -39,27 +38,27 @@ class EnrollBottle(webapp2.RequestHandler):
     
     logging.info( bottle_name )
     
-    bottle_code = PRNG(bottle_name)
+    bottle_code = PRNG(bottle_name)(256)
     
-    rng1 = PRNG()
+    rng1 = PRNG(datetime.datetime.now().microsecond)
     sk1 = SigningKey.generate(entropy=rng1)
     vk1 = sk1.get_verifying_key()
     
-    bottle_signature = sk.sign(bottle_code)
-    assert vk.verify(bottle_signature, bottle_code)
+    bottle_signature = sk1.sign(bottle_code)
+    assert vk1.verify(bottle_signature, bottle_code)
 
-    enclosure_code = PRNG(bottle_name)
-    rng2 = PRNG()
+    enclosure_code = PRNG(bottle_name)(256)
+    rng2 = PRNG(datetime.datetime.now().microsecond)
 
     sk2 = SigningKey.generate(entropy=rng2)
     vk2 = sk2.get_verifying_key()
     
-    enclosure_signature = sk.sign(enclosure_code)
-    assert vk.verify(enclosure_signature, enclosure_code)
+    enclosure_signature = sk2.sign(enclosure_code)
+    assert vk2.verify(enclosure_signature, enclosure_code)
     
-    bottle_label = (bottle_code, enclosure_signature)
-    label = (vk1, vk2)
-    enclosure_label = (bottle_signature, enclosure_code)
+    bottle_label = ( base58.b58encode( bottle_code ),  base58.b58encode( enclosure_signature ))
+    label = ( base58.b58encode( vk1.to_string() ), base58.b58encode( vk2.to_string()) )
+    enclosure_label = ( base58.b58encode( bottle_signature ), base58.b58encode( enclosure_code) )
     
     logging.info('bottle label: {0}'.format(bottle_label))
     logging.info('label: {0}'.format(label))
@@ -69,3 +68,4 @@ application = webapp2.WSGIApplication([
     ('/', MainPage),
     ('/enroll_bottle', EnrollBottle)
 ], debug=True)
+
