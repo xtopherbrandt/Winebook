9527cf84c5d558638cd28763f9141fba5bf8bd5e
Christopher Brandt <xtopher.brandt@gmail.com>
2015-02-05 23:29:25 +0000

    clean up UI. Populate validation fields after enroll.

--- js/app.js
+++ js/app.js
@@ -19,12 +19,19 @@ var main = function ()
           var bottle_tuple = identity.bottle_tuple;
           var label_tuple = identity.label_tuple;
           var enclosure_tuple = identity.enclosure_tuple;
-          $('<p>').text(bottle_tuple.bottle_code).addClass('code').appendTo('.bottle-code');
-          $('<p>').text(bottle_tuple.enclosure_signature).addClass('code').appendTo('.enclosure-signature');
-          $('<p>').text(label_tuple.bottle_key).addClass('code').appendTo('.bottle-verification-key');
-          $('<p>').text(label_tuple.enclosure_key).addClass('code').appendTo('.enclosure-verification-key');
-          $('<p>').text(enclosure_tuple.bottle_signature).addClass('code').appendTo('.bottle-signature');
-          $('<p>').text(enclosure_tuple.enclosure_code).addClass('code').appendTo('.enclosure-code');
+          $('.bottle-identity .bottle-code').text(bottle_tuple.bottle_code);
+          $('.bottle-identity .enclosure-signature').text(bottle_tuple.enclosure_signature);
+          $('.bottle-identity .bottle-verification-key').text(label_tuple.bottle_key);
+          $('.bottle-identity .enclosure-verification-key').text(label_tuple.enclosure_key);
+          $('.bottle-identity .bottle-signature').text(enclosure_tuple.bottle_signature);
+          $('.bottle-identity .enclosure-code').text(enclosure_tuple.enclosure_code);
+          
+          $('.identity-verification .bottle-code').val(bottle_tuple.bottle_code);
+          $('.identity-verification .enclosure-signature').val(bottle_tuple.enclosure_signature);
+          $('.identity-verification .bottle-verification-key').val(label_tuple.bottle_key);
+          $('.identity-verification .enclosure-verification-key').val(label_tuple.enclosure_key);
+          $('.identity-verification .bottle-signature').val(enclosure_tuple.bottle_signature);
+          $('.identity-verification .enclosure-code').val(enclosure_tuple.enclosure_code);
         }
       });
     });
